4b320f5735b892fabe23551c3cb559c2839b698d
HAMOUCH-Younes <youneshamouch9@gmail.com>
2025-05-17 11:57:01 +0100

    Connect to database Mongodb

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const mongoose = require('mongoose');
+
+
+
 
 app.use(express.json());
 
@@ -11,5 +15,22 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/Chat_Bot', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB ✅');
+})
+.catch((err) => {
+  console.error('MongoDB connection error ❌:', err);
+});
+
+
+
 
 
